1f8ed576550d345e49d1aafa4fb5aac0e7cf0be2
JuneMutten <junemutten@gmail.com>
2023-12-18 21:17:03 +0000

    Solution to advanced tasks

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+
+    except Exception as err:
+        result = None
+        print("Exception: {}".format(err), file=sys.stderr)
+
+    finally:
+        return result
