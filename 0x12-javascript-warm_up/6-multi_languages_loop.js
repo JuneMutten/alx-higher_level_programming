111dcdc8b1a092e4ebae80d2ba7c3f8ec7713170
JuneMutten <junemutten@gmail.com>
2024-03-13 23:08:14 -0700

    Checking standard for task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,6 @@
 
 const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < myArr.length; i++) 
-{
+for (let i = 0; i < myArr.length; i++) {
   console.log(myArr[i]);
 }
