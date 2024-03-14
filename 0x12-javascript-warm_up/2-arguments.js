93ef58354f3c73396eb57f091b85f751fb21b042
JuneMutten <junemutten@gmail.com>
2024-03-13 22:47:55 -0700

    Checking standard for task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,15 +2,10 @@
 
 const argumentCounter = process.argv.length;
 
-if (argumentCounter === 2)
-{
-    console.log('No argument');
-}
-else if (argumentCounter === 3)
-{
-    console.log('Argument found');
-}
-else 
-{
-    console.log('Arguments found');
+if (argumentCounter === 2) {
+  console.log('No argument');
+} else if (argumentCounter === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
