370847459e7d4754b771e76d789ab9d1d11490eb
JuneMutten <junemutten@gmail.com>
2024-03-18 00:30:36 -0700

    Solution to task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Module that connects python script to a database
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    # Database connection using command-line arguments
+    my_db = MySQLdb.connect(host="localhost", user=argv[1], password=argv[2],
+                            db=argv[3], port=3306)
+
+    # Creating query and executing query
+    my_cursor = my_db.cursor()
+    my_cursor.execute("SELECT * FROM states ORDER BY id")
+
+    # Fetches data from the query
+    my_data = my_cursor.fetchall()
+
+    # Iterating
+    for data in my_data:
+        print(data)
+
+    # Closing cursors and databases
+    my_cursor.close()
+    my_db.close()
