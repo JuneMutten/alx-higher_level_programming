b0892855e0d2ea77232b2ada034049a77ea8a3d9
JuneMutten <junemutten@gmail.com>
2024-03-18 00:56:29 -0700

    Tring to find duplicate issue

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,12 +10,12 @@ import MySQLdb
 if __name__ == "__main__":
 
     # Database connection using command-line arguments
-    my_db = MySQLdb.connect(host="localhost", user=argv[1], password=argv[2],
+    my_db = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2],
                             db=argv[3], port=3306)
 
     # Creating query and executing query
     my_cursor = my_db.cursor()
-    my_cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    my_cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
 
     # Fetches data from the query
     my_data = my_cursor.fetchall()
